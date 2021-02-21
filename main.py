82bee173832ca204e0ddc7fccb1a2740564a9033
abrorbek <abrorbekgithub@gmail.com>
2021-02-21 21:20:26 +0500

    last exchanges

--- main.py
+++ main.py
@@ -5,12 +5,35 @@ from flask import Flask
 
 app=Flask(__name__)
 
+html='''
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Computer Code</h1>
+<h3> <p>Some programming code:</p> </h3>
+
+<code>
+x = 5;
+y = 6;
+z = x + y;
+</code>
+
+</body>
+</html>
+'''
+
 @app.route('/')
 def hello():
     return 'hello'
 
 @app.route('/home')
 def home():
-    return 'home page'
-    
+    return html
+
+@app.route('/index')
+def index():
+    return "index"
+
+
 app.run(debug=True)
